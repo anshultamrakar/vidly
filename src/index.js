451be028f14931e298558f78d439071e8a3702ec
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-12-20 20:36:31 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from "./App"
 // import Counter from './Components/Counter';
-// import Counters from './Components/Counters';
-import MetaMask from './Components/MetaMask';
+// // import Counters from './Components/Counters';
+// import MetaMask from './Components/MetaMask';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 
 ReactDOM.render(
-  <MetaMask />,
+  <App />,
   document.getElementById('root')
 );
